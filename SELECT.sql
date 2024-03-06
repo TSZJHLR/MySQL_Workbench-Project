52abd7ed90dc4e429b966e50d04c6b6bb4ab04b9
Sujal Ratna Tuladhar <127120496+TSZJHLR@users.noreply.github.com>
2024-03-06 21:41:51 +0545

    SELECT.sql

---
+++ SELECT.sql
@@ -0,0 +1,11 @@
+USE HRMS;
+
+SELECT * FROM Guest;
+SELECT * FROM Room;
+SELECT * FROM Restaurant;
+SELECT * FROM Reservation;
+SELECT * FROM Inventory;
+SELECT * FROM Staff;
+SELECT * FROM Facilities;
+SELECT * FROM Cancellation;
+SELECT * FROM Payment;
